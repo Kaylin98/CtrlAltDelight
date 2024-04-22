e0f9dfbd26ba3838b07beff4dc7fb3e9dcc5d286
Kaylin Maharaj <Kaylin.Maharaj@sovtech.com>
2024-04-22 16:08:02 +0200

    add implementation for listing categories

--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Category } from '../models/category.model';
+import { CategoryService } from './../services/category.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
   styleUrls: ['./category-list.component.css']
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit,OnDestroy {
 
+  categories?: Category[];
+
+  constructor(private categoryService: CategoryService){
+  }
+  
+
+
+  ngOnInit(): void {
+    this.categoryService.getAllCategories()
+    .subscribe({
+      next: (response) =>{
+        this.categories = response;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
+  
 }
