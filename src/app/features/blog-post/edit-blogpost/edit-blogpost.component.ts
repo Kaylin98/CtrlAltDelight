02bcbe22019605877143d34271381e8308c0f33c
Kaylin Maharaj <Kaylin.Maharaj@sovtech.com>
2024-04-23 12:49:47 +0200

    edit implementation for blog post

---
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -0,0 +1,89 @@
+import { Component } from '@angular/core';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
+import { BlogPost } from '../models/blog-post.model';
+import { Category } from '../../category/models/category.model';
+import { Observable, Subscription } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BlogPostService } from '../services/blog-post.service';
+import { CategoryService } from '../../category/services/category.service';
+
+@Component({
+  selector: 'app-edit-blogpost',
+  templateUrl: './edit-blogpost.component.html',
+  styleUrls: ['./edit-blogpost.component.css']
+})
+export class EditBlogpostComponent {
+
+  id: string | null = null;
+  model?: BlogPost;
+  categories$? : Observable<Category[]>;
+  selectedCategories?: string[]
+
+
+  routeSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
+  getBlogPostSubscription?: Subscription;
+
+
+  constructor(private route: ActivatedRoute,
+    private blogPostService: BlogPostService,
+    private categoryService: CategoryService,
+    private router:Router) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+
+
+    this.routeSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+
+        // Get BlogPost From API
+        if (this.id) {
+          this.getBlogPostSubscription = this.blogPostService.getBlogPostById(this.id).subscribe({
+            next: (response) => {
+              this.model = response;
+              this.selectedCategories = response.categories.map(x => x.id);
+            }
+          });
+          ;
+        }
+
+      }
+    });
+  }
+
+  onFormSubmit(): void {
+    // Convert this model to Request Object
+    if (this.model && this.id) {
+      var updateBlogPost: UpdateBlogPost = {
+        author: this.model.author,
+        content: this.model.content,
+        shortDescription: this.model.shortDescription,
+        featuredImageUrl: this.model.featuredImageUrl,
+        isVisible: this.model.isVisible,
+        publishedDate: this.model.publishedDate,
+        title: this.model.title,
+        urlHandle: this.model.urlHandle,
+        categories: this.selectedCategories ?? []
+      };
+
+      this.updateBlogPostSubscription = this.blogPostService.updateBlogPost(this.id, updateBlogPost)
+      .subscribe({
+        next: (response) => {
+          this.router.navigateByUrl('/admin/blogposts');
+        }
+      });
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
+    this.getBlogPostSubscription?.unsubscribe();
+  }
+}
